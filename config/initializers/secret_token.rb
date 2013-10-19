b50df9fff68e2781c72c2b6727c398e41757f128
Will Spearing <nwspearing@gmail.com>
2013-10-18 17:29:45 -0700

    wdi - project 2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Whatnow::Application.config.secret_key_base = '187a711a0a67df425db522d144d6604f25b8046667829b366c0462c629e0fddf016784bf357727758cfacc0fd728c6b34b6a7126551c279f37c23dde28760569'
